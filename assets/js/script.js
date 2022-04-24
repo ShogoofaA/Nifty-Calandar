b9f178a31837732e73408ca16e846a65f2ddbaaa
Shogoofa Akbari <shogoofa.musket@gmail.com>
2022-04-23 23:39:00 -0700

    added local storage to save text info on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,35 @@
 const today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+const currenthour = moment().format('h');
+
+
+// create an function to save information from the textarea to my localstorage
+$('.saveBtn').on('click', saveText);
+
+function saveText() {
+    let saved = $(this).siblings('textarea').val();
+    let savedID = $(this).siblings('textarea').attr('id');
+    localStorage.setItem(saved, savedID)
+    console.log('blargh')
+}
+
+function getInfo() {
+    $('#9').val(localStorage.getItem('9'))
+    $('#10').val(localStorage.getItem('10'))
+    $('#11').val(localStorage.getItem('11'))
+    $('#12').val(localStorage.getItem('12'))
+    $('#1').val(localStorage.getItem('1'))
+    $('#2').val(localStorage.getItem('2'))
+    $('#3').val(localStorage.getItem('3'))
+    $('#4').val(localStorage.getItem('4'))
+    $('#5').val(localStorage.getItem('5'))
+}
+
+
+// check time by the hour and add a class of past present or future
+
+function currentTime() {
+    if (currenthour == 'h') {
+        addClass('.present')
+    }
+}
